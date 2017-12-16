3c0dfeb6a155ca111f49ac8dc34209df049aa581
AraceliCumen <aycuevam@gmail.com>
2017-12-16 01:33:26 -0500

    Actualizando Maps

--- js/mapadesktop.js
+++ js/mapadesktop.js
@@ -1,6 +1,6 @@
 function initMap() {
     var uluru = {lat: -12.0603081, lng: -77.0415133};
-    var map = new google.maps.Map(document.getElementById('map'), {
+    var map = new google.maps.Map(document.getElementById('mapdescktop'), {
       zoom: 4,
       center: uluru
     });
